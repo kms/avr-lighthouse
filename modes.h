36e18e62d8dcd5b578b0b6873a19a4d47768c52f
Karl-Martin Skontorp <kms@skontorp.net>
2011-08-13 14:06:00 +0200

    Update homepage reference.

--- modes.h
+++ modes.h
@@ -1,7 +1,7 @@
 /* avr-lighthouse
  * Lighthouse controller.
  *
- * (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://picofarad.org/
+ * (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://22pf.org/
  * Licensed under the GNU GPL 2.0 or later.
  */
 
